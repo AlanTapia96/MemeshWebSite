e660fabbc5ee1c8ace6b73406a4a0a868c40c3c5
AlanTapia96 <tapia.alan.g@gmail.com>
2021-06-28 14:37:03 -0300

    carousel automatico

--- js/carousel.js
+++ js/carousel.js
@@ -1,29 +1,8 @@
 const d = document;
 
 export default function carousel(direction,divChilds){
-    
-    // if(direction == 'left'){
-    //     l--;
-    //     for (const i of divChilds) {
-    //         if(l==0) i.style.left = '0px';
-    //         if(l==1) i.style.left = '-37rem';
-    //         if(l==2) i.style.left = '-75rem';
-    //         if(l==3) i.style.left = '-112.5rem';
-    //         if(l<0) l=0;
-    //     };
-    // }else if(direction == 'right'){
-    //     l++;
-    //     for (const i of divChilds) {
-    //         if(l==0) i.style.left = '0px';
-    //         if(l==1) i.style.left = '-37rem';
-    //         if(l==2) i.style.left = '-75rem';
-    //         if(l==3) i.style.left = '-112.5rem';
-    //         if(l==4) i.style.left = '-145rem';
-    //         if(l>4) l=4;
-    //     }
-    // }
+
     if(direction == 'left'){
-        console.log("left");
         for (const div of divChilds) {
             let valor = getComputedStyle(div).left.slice(0,-2);
             let convertido = ((parseFloat(valor) / 16) + 37);
--- js/main.js
+++ js/main.js
@@ -3,63 +3,19 @@ import carousel from "./carousel.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e =>{
-    let l = 0;
+    // let l = 0;
 
     d.addEventListener('click',(e)=>{
-        let direction = e.target.parentElement.classList[1];
-        let divChilds = e.target.parentElement.parentElement.childNodes[5].querySelectorAll('div');
-        carousel(direction,divChilds);
+        let parentElement = e.target.parentElement;
+        if(parentElement.classList[1] == "right" || parentElement.classList[1] == "left"){
+            let direction = e.target.parentElement.classList[1];
+            let divChilds = e.target.parentElement.parentElement.nextSibling.nextSibling.querySelectorAll('div');
+            carousel(direction,divChilds);
+        }
     })
+
+
 
 })
 
 
-
-
-
-
-
-
-
-
-
-// // document.addEventListener("DOMContentLoaded", e =>{
-//     let span = document.querySelectorAll('#carousel-others .span');
-//     let div = document.querySelectorAll('#carousel-others_imgs div');
-
-//     let prueba = span.div;
-//     console.log(prueba);
-
-
-//     console.log(div);
-//     let l = 0;
-
-
-//     span[0].onclick = () => {
-//         l--;
-
-//         for (const i of div) {
-//             if(l==0) i.style.left = '0px';
-//             if(l==1) i.style.left = '-37rem';
-//             if(l==2) i.style.left = '-75rem';
-//             if(l==3) i.style.left = '-112.5rem';
-//             if(l<0) l=0;
-//         };
-       
-//     }
-    
-//     span[1].onclick = () => {
-//         l++;
-//         for (const i of div) {
-//             if(l==0) i.style.left = '0px';
-//             if(l==1) i.style.left = '-37rem';
-//             if(l==2) i.style.left = '-75rem';
-//             if(l==3) i.style.left = '-112.5rem';
-//             if(l==4) i.style.left = '-145rem';
-//             if(l>4) l=4;
-//         }
-        
-//     }
-    
-    
-// // })
