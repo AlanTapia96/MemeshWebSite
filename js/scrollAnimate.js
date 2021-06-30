6c5c3821910d56b30bece24d0efbb774dd2f0279
AlanTapia96 <tapia.alan.g@gmail.com>
2021-06-30 12:02:43 -0300

    scrollreveal

---
+++ js/scrollAnimate.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#about .col-4',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+})
+
+sr.reveal('#about .col-8',{
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+})
+
+
+
+
